3e408acf680ca982682513470bdfdfb2e7ddf24f
joe960913 <59116882@qq.com>
2019-11-09 13:06:55 +0800

    add

--- foundationType.ts
+++ foundationType.ts
@@ -61,7 +61,13 @@ let u: undefined = undefined;
 
 //Never:表示那些永不存在的值的类型
 //never类型是那些总是会抛出异常或根本就不会有返回值的函数表达式或箭头函数表达式的返回值类型
-function error(message:string):never{
-    throw new Error(message)
+function error(message: string): never {
+  throw new Error(message);
 }
+
+//Object:表示非原始类型，也就是除number，string，boolean，symbol，null或undefined之外的类型。如对象。
+
+declare function create(o: object | null): void;
+create({ x: 0 });
+create(null);
 
